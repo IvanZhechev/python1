6a4f333f513e2c4c47e5027543f91a57629513fc
Nerc <seaways414@gmail.com>
2024-09-21 14:40:09 +0300

    python_2.py

--- python_2.py
+++ python_2.py
@@ -1,5 +1,16 @@
 
-number = input("Введіть 5-значне число: ")
 
-reversed_number = number[::-1]
+number = int(input("Введіть 5-значне число: "))
+
+
+reversed_number = 0
+
+
+for _ in range(5):
+    digit = number % 10
+    reversed_number = reversed_number * 10 + digit
+    number = number // 10
+
+
 print(reversed_number)
+
