2a11812f21b23797829709a3d8b6fa6b74f26e22
Nerc <seaways414@gmail.com>
2024-09-21 13:29:49 +0300

    python_2.py

--- python_1.py
+++ python_1.py
@@ -1,1 +1,13 @@
-print ("Hello World")
+
+number = int(input("Введіть 4-значне число: "))
+
+thousands, remainder = divmod(number, 1000)
+print(thousands)
+
+hundreds, remainder = divmod(remainder, 100)
+print(hundreds)
+
+tens, units = divmod(remainder, 10)
+print(tens)
+
+print(units)
