60e2a378530522a65523de7d11be53db677cb145
Nerc <seaways414@gmail.com>
2024-10-03 18:34:23 +0300

    lisseon 5.1

--- lisseon 5.py
+++ lisseon 5.py
@@ -2,12 +2,16 @@ import string
 import keyword
 
 def is_valid_variable_name(name):
+
     if name in keyword.kwlist:
         return False
+
     if name[0].isdigit():
         return False
+
     if " " in name or name.count("_") > 1:
         return False
+
     for char in name:
         if char in string.punctuation and char != "_":
             return False
@@ -15,6 +19,8 @@ def is_valid_variable_name(name):
             return False
 
     return True
+
+
 examples = ["_", "__", "my_new_value", "get_Value", "getValue", "3m", "m3", "assert", "assert_exception"]
 
 results = {example: is_valid_variable_name(example) for example in examples}
