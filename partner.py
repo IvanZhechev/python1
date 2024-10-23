4d34a9c058a7bae9ee707f5929d26096393c9e96
Nerc <seaways414@gmail.com>
2024-10-23 16:00:26 +0300

    partner.py

---
+++ partner.py
@@ -0,0 +1,15 @@
+def is_even(digit):
+    """
+    Перевірка чи є число парним.
+
+    :param digit: Ціле число
+    :return: True якщо число парне, False якщо непарне
+    """
+    return digit % 2 == 0
+
+
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
